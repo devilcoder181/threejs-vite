2cce4ef7aa92dd7e8a2f9f0ce2e0e4adf1caa659
alan <devilcoder181@gmail.com>
2021-11-24 14:50:52 +0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import './assets/scss/app.scss'
+import * as THREE from 'three';
+
+const holder = document.getElementById('scene');
+
+const scene: any = new THREE.Scene();
+
+const geometry: void = new THREE.BoxGeometry(1, 1, 1);
+const material: void = new THREE.MeshBasicMaterial({ color: 0x7a67ff });
+const square: void = new THREE.Mesh(geometry, material)
+scene.add(square);
+
+// Sizes
+const size: Object = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// Camera
+const camera: void = new THREE.PerspectiveCamera(55, size.width / size.height);
+camera.position.z = 3
+scene.add(camera);
+
+// Renderer
+
+const renderer: any = new THREE.WebGLRenderer({
+    canvas: holder
+})
+
+renderer.setSize(size.width, size.height)
+
+renderer.render(scene, camera)
